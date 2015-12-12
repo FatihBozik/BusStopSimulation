6f5b4bb4799c11d3c93a92f4559d07e0da2d8d0e
fatihbozik <conqueror.05@gmail.com>
2015-12-12 20:28:25 +0200

    Başlangıç ekranı arayüzü tasarlandı

--- app/src/main/java/com/fatihbozik/busstopsimulation/MainActivity.java
+++ app/src/main/java/com/fatihbozik/busstopsimulation/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fatihbozik.busstopsimulation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -17,15 +18,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnContinue = (Button) findViewById(R.id.button_continue);
+        Button btnContinue = (Button) findViewById(R.id.button_next);
         btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 txtMaxBusCount = (EditText) findViewById(R.id.txt_maxBusCount);
-                maxBusCount = Integer.parseInt(txtMaxBusCount.toString());
+                maxBusCount = Integer.parseInt(txtMaxBusCount.getText().toString());
 
                 txtBusStopCount = (EditText) findViewById(R.id.txt_busStopCount);
-                busStopCount = Integer.parseInt(txtBusStopCount.toString());
+                busStopCount = Integer.parseInt(txtBusStopCount.getText().toString());
+
+                Intent stopIntent = new Intent(MainActivity.this, StopActivity.class);
+                stopIntent.putExtra("maxBusCount", maxBusCount);
+                stopIntent.putExtra("busStopCount", busStopCount);
+                startActivity(stopIntent);
             }
         });
     }
